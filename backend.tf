52a7e613c8b2e44d2e9b3771702fb642c32199dc
gh-actions <github-actions@users.noreply.github.com>
2025-07-02 09:03:53 +0000

    Add backend.tf for remote state [skip ci]

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "devops-week9-rg"
-    storage_account_name = "tfstateweek916020725437"
+    storage_account_name = "tfstateweek916020817068"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
